830936dd0d4d91c633ef40af7349af0994cbffb8
LordOverLord0 <rofyorug@gmail.com>
2019-08-03 19:19:19 +0300

    Переместил все файлы в /src/

---
+++ example/Config/Command.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Данный файл нужен для регистрации команд бота
+ */
+
+Handler::IgnoreMode(true, '/'); // Включает игнор мод. Бот будет игнорить любые сообщения, без "/" в начале
+
+Handler::Register('hello-world', function () {
+    return 'Hello world';
+});
+
+
