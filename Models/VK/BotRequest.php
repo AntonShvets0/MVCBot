5a0a36984aec2bf83e0f25206db176ab74790e48
LordOverLord0 <rofyorug@gmail.com>
2019-08-03 17:57:05 +0300

    Наполнил класс BotRequest

--- Models/VK/BotRequest.php
+++ Models/VK/BotRequest.php
@@ -14,9 +14,20 @@ class BotRequest
      * @return array
      * Отправляет запрос на VK API
      */
-    public function On($method, $data = [])
+    public static function On($method, $data = [])
     {
-        return [];
+        $data = array_merge($data, CONFIG['vkSendData']);
+
+        Logger::Info("Send request https://api.vk.com/method/{$method}, data: " . json_encode($data));
+
+        $response = json_decode(self::SendPost('https://api.vk.com/method/' . $method, $data), true);
+
+        if (isset($response['error'])) {
+            Logger::Error("Response error(#{$response['error']['error_code']}): {$response['error']['error_msg']}");
+            return [];
+        }
+
+        return $response['response'];
     }
 
     /**
@@ -25,16 +36,46 @@ class BotRequest
      * @return false|string
      * Отправляет POST запрос на URL, и возвращает результат
      */
-    protected function SendPost($url, $data = [])
+    protected static function SendPost($url, $data = [])
     {
-        return false;
+        if (!is_string($data)) {
+            $data = http_build_query($data);
+        }
+
+        if (self::UseCurl()) {
+            $ch = curl_init();
+
+            curl_setopt($ch, CURLOPT_POST, true);
+
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+            curl_setopt($ch, CURLOPT_HEADER, false); // Убираем из результата заголовок HTTP
+
+            curl_setopt($ch, CURLOPT_URL, $url); // Ссылка
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data); // POST данные
+
+            $response = curl_exec($ch);
+            curl_close($ch);
+        } else { // Если cURL не установлен, то выполняем запрос через file_get_contents
+            $header = ['http' =>
+                [
+                    'method'  => 'POST',
+                    'header'  => 'Content-Type: application/x-www-form-urlencoded',
+                    'content' => $data
+                ]
+            ];
+
+            $response = file_get_contents($url, false, stream_context_create($header));
+        }
+
+        return $response;
     }
 
     /**
      * @return bool
      * Проверка на cURL
      */
-    private function UseCurl()
+    private static function UseCurl()
     {
         return extension_loaded('curl');
     }
