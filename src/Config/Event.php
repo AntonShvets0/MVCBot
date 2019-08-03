1cb6846254acae5e563eee258f209fe1e94cda91
LordOverLord0 <rofyorug@gmail.com>
2019-08-03 20:10:51 +0300

    README.md

--- src/Config/Event.php
+++ src/Config/Event.php
@@ -11,3 +11,21 @@ Event::Register('confirmation', function () {
 Event::Register('message_new', function () {
     Handler::Run();
 });
+
+
+$keyBoard = [
+    true, // Скрывать ли клавиатуру после нажатия. false/true
+
+    [ // первая строка
+        "Первая кнопка" => [ // Название кнопки
+            "clr" => "primary", // цвет,
+            "cmd" => ["hello", "world"] // payload
+        ]
+    ],
+    [ // Вторая строка
+        "Вторая кнопка" => [ // Название кнопки
+            "clr" => "negative", // цвет,
+            "cmd" => ["hello", "world2"] // payload
+        ]
+    ]
+];
